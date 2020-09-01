eaae9e09b274872b3289ad433a82ed7705c670f3
Dendi Handian <handian.dendi@gmail.com>
2020-09-01 21:17:59 +0700

    basic items endpoints

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+
+class Items(Resource):
+    def get(self):
+        return {'items': items}, 200
+
+    def post(self):
+        data = request.get_json()
+        if next(filter(lambda x: x['name'] == data['name'], items), None):
+            return {'message': "An item with name '{}' already exists".format(data['name'])}, 400
+        item = {'name': data['name'], 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+
+api.add_resource(Item, '/items/<string:name>')
+api.add_resource(Items, '/items')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
