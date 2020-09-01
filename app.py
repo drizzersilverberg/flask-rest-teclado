0c2b5cae9a750c121cf059abfb7a04e56756f06b
Dendi Handian <handian.dendi@gmail.com>
2020-09-01 22:24:57 +0700

    reqparser

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -23,7 +23,20 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        parser = reqparse.RequestParser()
+        parser.add_argument('name',
+                            type=str,
+                            required=True,
+                            help="This field cannot be left blank."
+                            )
+        parser.add_argument('price',
+                            type=float,
+                            required=True,
+                            help="This field cannot be left blank."
+                            )
+
+        data = parser.parse_args()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': data['name'], 'price': data['price']}
