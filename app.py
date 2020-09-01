f41fa74936e7df49c1c4d56c8df4d4ab245a76e8
Dendi Handian <handian.dendi@gmail.com>
2020-09-01 22:16:11 +0700

    put item

--- app.py
+++ app.py
@@ -22,6 +22,16 @@ class Item(Resource):
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'Item deleted'}
 
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': data['name'], 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 
 class Items(Resource):
     def get(self):
