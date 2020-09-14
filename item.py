8de898486b39dc3597311b219e3a5dd0438fc2b4
Dendi Handian <handian.dendi@gmail.com>
2020-09-14 21:50:40 +0700

    writing item resource to db

--- item.py
+++ item.py
@@ -16,8 +16,8 @@ class Item(Resource):
                         help="This field cannot be left blank."
                         )
 
-    @jwt_required()
-    def get(self, name):
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
         query = "SELECT * FROM items WHERE name=?"
@@ -28,10 +28,14 @@ class Item(Resource):
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
 
-        return {'message': 'Item not found'}, 404
+        return None
 
-        # item = next(filter(lambda x: x['name'] == name, items), None)
-        # return {'item': item}, 200 if item else 404
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
 
     def delete(self, name):
         global items
@@ -68,9 +72,19 @@ class Items(Resource):
 
     def post(self):
         data = Item.parser.parse_args()
-        # data = request.get_json()
-        if next(filter(lambda x: x['name'] == data['name'], items), None):
-            return {'message': "An item with name '{}' already exists".format(data['name'])}, 400
+
+        if Item.find_by_name(data['name']):
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+
         item = {'name': data['name'], 'price': data['price']}
-        items.append(item)
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?,?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
         return item, 201
