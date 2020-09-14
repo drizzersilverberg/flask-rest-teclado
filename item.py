4a748f462f404aae9426ca35bc98cb21a4a60b54
Dendi Handian <handian.dendi@gmail.com>
2020-09-14 22:31:43 +0700

    retrieving item list from db

--- item.py
+++ item.py
@@ -105,7 +105,18 @@ class Items(Resource):
                         )
 
     def get(self):
-        return {'items': []}, 200
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+
+        return {'items': items}, 200
 
     def post(self):
         data = Item.parser.parse_args()
