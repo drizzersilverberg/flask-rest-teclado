621e92b83be5567dc168708448f671987b3efd66
Dendi Handian <handian.dendi@gmail.com>
2020-09-14 23:50:05 +0700

    improve project structures

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
-from user import UserRegister
-from item import Item, Items
+from resources.user import UserRegister
+from resources.item import Item, Items
 
 app = Flask(__name__)
 app.secret_key = 'dendi'
