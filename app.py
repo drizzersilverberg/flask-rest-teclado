e729fef597288316102dc33ac01b95c9ab1a8994
Dendi Handian <handian.dendi@gmail.com>
2020-09-15 00:43:51 +0700

    sqlalchemy tables

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT, jwt_required
+from db import db
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, Items
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'dendi'
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
@@ -17,4 +19,5 @@ api.add_resource(Items, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
