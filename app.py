cdc4d3d82359b8cbc5356272a86322a04f131570
Dendi Handian <handian.dendi@gmail.com>
2020-09-18 01:02:55 +0700

    no more creating tables

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'dendi'
 api = Api(app)
-jwt = JWT(app, authenticate, identity)
 
-items = []
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/items/<string:name>')
 api.add_resource(Items, '/items')
