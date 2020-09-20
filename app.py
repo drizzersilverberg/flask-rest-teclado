a608b6b76e03ab0461f0b00be910fce7f44ded75
Dendi Handian <handian.dendi@gmail.com>
2020-09-21 04:20:36 +0700

    implement blacklist

--- app.py
+++ app.py
@@ -12,11 +12,14 @@ from resources.item import Item, Items
 from models.store import StoreModel
 from models.item import ItemModel
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.secret_key = 'dendi'
 api = Api(app)
 
@@ -34,6 +37,11 @@ def add_claims_to_jwt(identity):
     if (identity == 1):
         return {'is_admin': True}
     return {'is_admin': False}
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['identity'] in BLACKLIST
 
 
 @jwt.expired_token_loader
