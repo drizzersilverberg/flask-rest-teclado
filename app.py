d8db4ac5c94ce697f47e5859886316a766770275
Dendi Handian <handian.dendi@gmail.com>
2020-09-21 02:11:43 +0700

    logging in with flask jwt extended

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT, jwt_required
+from flask_jwt_extended import JWTManager
 from db import db
-from security import authenticate, identity
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, Items
 from models.store import StoreModel
 from models.item import ItemModel
@@ -16,11 +15,13 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'dendi'
 api = Api(app)
 
+
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-jwt = JWT(app, authenticate, identity)
+
+jwt = JWTManager(app)
 
 api.add_resource(Store, '/stores/<int:id>')
 api.add_resource(StoreList, '/stores')
@@ -28,6 +29,7 @@ api.add_resource(Item, '/items/<string:name>')
 api.add_resource(Items, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/users/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     db.init_app(app)
