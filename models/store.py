181f63cc139253861d08a2f4a6284327cb9a7f2d
Dendi Handian <handian.dendi@gmail.com>
2020-09-27 20:31:45 +0700

    add unique to item and store models

--- models/store.py
+++ models/store.py
@@ -6,7 +6,7 @@ class StoreModel(db.Model):
     __tablename__ = 'stores'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
+    name = db.Column(db.String(80), unique=True)
     items = db.relationship('ItemModel', lazy='dynamic')
 
     def __init__(self, name):
@@ -15,7 +15,7 @@ class StoreModel(db.Model):
     def json(self):
         return {
             'id': self.id,
-            'name': self.name, 
+            'name': self.name,
             'items': [item.json() for item in self.items]
         }
 
